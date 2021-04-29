d12af8b9acdb42a5045702f03b08c79f4d280488
Evan Kysley <evan@kysley.com>
2021-04-28 23:57:51 -0400

    feat: basic implementation

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,63 @@
-export const sum = (a: number, b: number) => {
-  if ('development' === process.env.NODE_ENV) {
-    console.log('boop');
-  }
-  return a + b;
+const ignoredTerms = ['and'];
+
+const mul = (x: number) => (time: number) => x * time;
+
+const ms = mul(1000);
+
+const second = (num: number) => ms(num);
+
+const minute = (num: number) => num * ms(60);
+
+const hour = (num: number) => minute(num * 60);
+
+const day = (num: number) => hour(num * 24);
+
+const week = (num: number) => day(num * 7);
+
+const terms: { [term: string]: any } = {
+  hr: hour,
+  hrs: hour,
+  hour,
+  hours: hour,
+  min: minute,
+  mins: minute,
+  minute,
+  minutes: minute,
+  day,
+  days: day,
+  sec: second,
+  secs: second,
+  second,
+  seconds: second,
+  week,
+  weeks: week,
 };
+
+function tokenize(input: string) {
+  const tokens = input.split(' ');
+
+  return tokens.reduce<string[]>((acc, token) => {
+    if (!ignoredTerms.includes(token)) {
+      const stripped = token.replace(/\W/g, '');
+      acc.push(stripped);
+    }
+    return acc;
+  }, []);
+}
+
+function convert(input: string) {
+  const tokens = tokenize(input);
+
+  let ms = 0;
+  for (let i = 0; i <= tokens.length; i++) {
+    if (i % 2 === 0) {
+      if (Number(tokens[i])) {
+        const term = terms[tokens[i + 1]];
+        ms += term(tokens[i]);
+      }
+    }
+  }
+  return ms;
+}
+
+export { convert as t };
