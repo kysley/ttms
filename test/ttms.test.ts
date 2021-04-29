dd2beb24e4c395451ee3abadbdea383041bd6711
Evan Kysley <evan@kysley.com>
2021-04-28 23:57:55 -0400

    test: add em

---
+++ test/ttms.test.ts
@@ -0,0 +1,13 @@
+import { t } from '../src';
+
+describe('ttms', () => {
+  it('converts 1 hour 30 minutes to milliseconds', () => {
+    expect(t('1 hour and 30 min')).toEqual(5400000);
+  });
+  it('converts days', () => {
+    expect(t('2 days')).toEqual(172800000);
+  });
+  it('converts weeks', () => {
+    expect(t('1 week')).toEqual(604800000);
+  });
+});
